2431fa105ac9c084df942e0a675e64dff59f9144
BertrandF26 <bfreylin@gmail.com>
2017-02-22 19:59:58 +0100

    Img + Api

--- matching/models.py
+++ matching/models.py
@@ -2,15 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class Movies(models.Model):
     id = models.IntegerField(primary_key=True, db_column='MovieId')
     title = models.CharField(max_length=255)
     genres = models.CharField(max_length=255)
 
+
 class Links(models.Model):
     movie = models.ForeignKey(Movies, db_column='MovieId')
     imdb = models.CharField(max_length=20, db_column='imdbId')
     tmdb = models.CharField(max_length=20, db_column='tmdbId')
+
 
 class Ratings(models.Model):
     user = models.IntegerField(name='userId')
@@ -18,9 +21,11 @@ class Ratings(models.Model):
     rating = models.DecimalField(max_digits=2, decimal_places=1)
     date = models.IntegerField()
 
+
 class GenomeTags(models.Model):
     id = models.IntegerField(primary_key=True)
     tag = models.CharField(max_length=255)
+
 
 class GenomeScores(models.Model):
     movie = models.ForeignKey(Movies, db_column='MovieId')
--- matching/utils.py
+++ matching/utils.py
@@ -1,7 +1,12 @@
+import operator
+
+from django.db.models import Q
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from matching.models import Movies
+from matching.models import Movies, GenomeScores, Links
 import json
+import tmdbsimple as tmdb
+tmdb.API_KEY = '78d4f3bf87e46dd40677da443ef8ed15'
 
 def get_movies(request):
     if request.is_ajax():
@@ -22,5 +27,21 @@ def get_movies(request):
 
 @csrf_exempt
 def find_movies(request):
-    data = []
-    return HttpResponse(data)
+    data = request.POST
+    movie_id = data['the_post[id]']
+    movie = Movies.objects.get(id=movie_id)
+    movie_genres = movie.genres.split('|')
+    condition = reduce(operator.and_, [Q(genres__icontains=s) for s in movie_genres])
+    first_level = Movies.objects.filter(condition).exclude(id=movie_id).all()
+    tag_associated_movies = GenomeScores.objects.filter(movie_id=movie_id).select_related('tag')
+    links = Links.objects.filter(movie_id__in=[k.id for k in first_level[:10]]).all()
+    results = []
+    for movie in first_level[:10]:
+        movies_object = tmdb.Movies(int(links.get(movie_id=movie.id).tmdb)).info()
+        movies_selected_json = {}
+        movies_selected_json['poster'] = 'https://image.tmdb.org/t/p/w500'+movies_object['poster_path']
+        movies_selected_json['title'] = movies_object['title']
+        results.append(movies_selected_json)
+    data = json.dumps(results)
+    mimetype = 'application/json'
+    return HttpResponse(data, mimetype)
